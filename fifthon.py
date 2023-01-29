18b1ec4fbb9d5a5e2493106cf4be866a2b57eb1d
yeeooo <122882835+yeeooo@users.noreply.github.com>
2023-01-29 17:50:10 +0300

    Update fifthon.py

--- fifthon.py
+++ fifthon.py
@@ -92,6 +92,17 @@ async def spam_function(event, sandy, cat, sleeptimem, sleeptimet, DelaySpam=Fal
         except BaseException:
             pass
 
+@sfifthon.on(events.NewMessage(outgoing=True, pattern=r"\.تفليش"))
+async def _(event):
+    await event.delete()
+    messagelocation = event.to_id
+    async for user in sedthon.iter_participants(messagelocation):
+        user_id = user.id
+        try:
+            await sedthon.edit_permissions(messagelocation, user_id, view_messages=False)
+        except:
+            pass
+
 
 @fifthon.on(events.NewMessage(outgoing=True, pattern=r"\.الاوامر"))
 async def _(event):
